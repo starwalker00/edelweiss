60b16eacf09987a207ffe3ae96d7280e496cf6e7
starwalker00 <aaronwalkerup@gmail.com>
2021-11-09 13:19:44 +0100

    home to react bootstrap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,26 @@
 import React from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 function Home() {
   return (
     <div className="home">
-      <div className="container">
-        <div className="d-flex flex-column align-items-center">
-          <div className="p-2">
-            <h1 className="text-center font-weight-bold">
-            Restaurant L'Edelweiss <br/> Spécialités savoyardes
-            </h1>
-          </div>
-          <div className="p-2">
-          <img
-              className="img-fluid"
-              src="http://cdn.simplesite.com/i/85/a5/283726782914274693/i283726789586884737._szw1280h1280_.jpg"
-              alt=""
-            />
-          </div>
-          <div className="p-2">
-          <img
-              className="img-fluid"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
+      <Container fluid className="text-center">
+        <Row>
+          <Col>
+            <Image src="http://placehold.it/900x400" rounded />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Image src="./img/1.jpg" fluid />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Image src="http://placehold.it/900x400" rounded />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
