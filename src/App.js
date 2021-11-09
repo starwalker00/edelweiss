c31fdb65a38b9f72aa8250dbda1969c8d53ad4cb
starwalker00 <aaronwalkerup@gmail.com>
2021-11-09 11:23:13 +0100

    package: bootstrap and scss support

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// eslint-disable-next-line
 import { Navigation, Footer, Home, About } from "./components";
+import './scss/App.scss';
+import './scss/custom.scss';
+
 function App() {
   return (
     <div className="App d-flex p-2 flex-column align-self-center justify-content-center">
