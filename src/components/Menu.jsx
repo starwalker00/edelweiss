6a0bec98a74b57a9401550b4c6cdf35ce35e1f22
starwalker00 <aaronwalkerup@gmail.com>
2021-11-15 21:10:18 +0100

    app: start menu design

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
 function Menu() {
   return (
     <div className="menu">
+      <Navbar expand="lg" variant="light" bg="light" sticky="top">
+        <Container>
+          <Nav>
+            <Nav.Item>
+              <Nav.Link href="#carte">Carte</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#vins">Vins</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
+      </Navbar>
       <Container>
         <Row>
-          <Col>lundi</Col>
-          <Col>12h-13h30 | 19h30-22h</Col>
+          <Col>
+          <Image id="carte" className="img-fluid" src="./img/menu/la-carte_i283726789573158798._szw1280h1280_.jpg" />
+          </Col>
         </Row>
         <Row>
-          <Col>dimanche</Col>
-          <Col>fermé</Col>
-        </Row>        
+          <Image id="vins" className="img-fluid" src="./img/menu/vins1_i283726789586882369._szw1280h1280_.jpg" />
+        </Row>
+        <Row>
+          <Image className="img-fluid" src="./img/menu/vins2_i283726789573158456._szw1280h1280_.jpg" />
+        </Row>   
       </Container>
     </div>
   );
