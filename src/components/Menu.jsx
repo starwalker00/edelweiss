4d294dea590fe8eb946ac30d6a876772d50ec842
starwalker00 <aaronwalkerup@gmail.com>
2021-11-15 21:26:17 +0100

    app: menu images zoomables

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { Navbar, Nav } from "react-bootstrap";
 
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
 function Menu() {
   return (
     <div className="menu">
@@ -17,18 +20,28 @@ function Menu() {
           </Nav>
         </Container>
       </Navbar>
-      <Container>
+      <Container className="text-center">
         <Row>
           <Col>
-          <Image id="carte" className="img-fluid" src="./img/menu/la-carte_i283726789573158798._szw1280h1280_.jpg" />
+            <Zoom>
+              <Image id="carte" className="img-fluid" src="./img/menu/la-carte_i283726789573158798._szw1280h1280_.jpg" />
+            </Zoom>
           </Col>
         </Row>
         <Row>
-          <Image id="vins" className="img-fluid" src="./img/menu/vins1_i283726789586882369._szw1280h1280_.jpg" />
+          <Col>
+            <Zoom>
+              <Image id="vins" className="img-fluid" src="./img/menu/vins1_i283726789586882369._szw1280h1280_.jpg" />
+            </Zoom>
+          </Col>
         </Row>
         <Row>
-          <Image className="img-fluid" src="./img/menu/vins2_i283726789573158456._szw1280h1280_.jpg" />
-        </Row>   
+          <Col>
+            <Zoom>
+              <Image className="img-fluid" src="./img/menu/vins2_i283726789573158456._szw1280h1280_.jpg" />
+            </Zoom>
+          </Col>
+        </Row>        
       </Container>
     </div>
   );
