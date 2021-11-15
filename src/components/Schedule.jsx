2e0fc7d1c8a4285d7bf0a08c3c9bd9637249f56b
starwalker00 <aaronwalkerup@gmail.com>
2021-11-15 23:31:32 +0100

    app: fix schedule times responsive centering

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -4,38 +4,38 @@ import { Container, Row, Col } from "react-bootstrap";
 function Schedule() {
   return (
     <div className="schedule">
-      <Container className="">
+      <Container className="text-center">
         <Row>
           <Col>Heures d'ouverture</Col>
         </Row>
         <hr/>
         <Row>
-          <Col>lundi</Col>
-          <Col xs={8}>12h-13h30 | 19h30-22h</Col>
+          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>lundi</Col>
+          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
         </Row>
         <Row>
-          <Col>mardi</Col>
-          <Col xs={8}>12h-13h30 | 19h30-22h</Col>
+          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>mardi</Col>
+          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
         </Row>
         <Row>
-          <Col>mercredi</Col>
-          <Col xs={8}>12h-13h30 | 19h30-22h</Col>
+          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>mercredi</Col>
+          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
         </Row>
         <Row>
-          <Col>jeudi</Col>
-          <Col xs={8}>12h-13h30 | 19h30-22h</Col>
+          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>jeudi</Col>
+          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
         </Row>
         <Row>
-          <Col>vendredi</Col>
-          <Col xs={8}>12h-13h30 | 19h30-22h30</Col>
+          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>vendredi</Col>
+          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h30</Col>
         </Row>
         <Row>
-          <Col>samedi</Col>
-          <Col xs={8}>19h30-22h30</Col>
+          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>samedi</Col>
+          <Col xs={8} md={6} lg={4}>19h30-22h30</Col>
         </Row>
         <Row>
-          <Col>dimanche</Col>
-          <Col xs={8}>fermé</Col>
+          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>dimanche</Col>
+          <Col xs={8} md={6} lg={4}>fermé</Col>
         </Row>        
       </Container>
     </div>
