6700ec0783ebdf40f0bfcdf0f049db8fb32af4e2
starwalker00 <aaronwalkerup@gmail.com>
2021-11-17 20:57:10 +0100

    app: home - add headings

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,15 @@ import MyCarousel from "./MyCarousel";
 function Home() {
   return (
     <div className="home">
-      <Container fluid className="text-center">
+      <Container fluid="md" className="text-center">
         <Row className="mt-4">
-          <Col>
+          <Col xs={12} md={8} className="m-auto">
+            <h1>Restaurant Savoyard à Rennes</h1>
+            <p class="lead">
+              Dans un cadre chaleureux, goûtez les principales recettes savoyardes et vivez un moment innoubliable.
+            </p>
+          </Col>
+          <Col xs={12} md={4} className="m-auto">
             <Image src="./img/1.jpg" fluid />
           </Col>
         </Row>
