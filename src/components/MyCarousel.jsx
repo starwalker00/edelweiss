6d8a60c1e42a87daa92d423cd98b8a459b4a08cf
starwalker00 <aaronwalkerup@gmail.com>
2021-11-17 20:30:46 +0100

    app: add carousel images example

--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 
 function MyCarousel() {
   return (
     <div className="mycarousel">
       <Container className="text-center"> 
         <Row>
-          <Col>mycarousel</Col>
+          <Col>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="./img/carousel/i283726789463333744._szw1280h1280_.jpg"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="./img/carousel/i283726789463333625._szw1280h1280_.jpg"
+                  alt="Second slide"
+                />
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
         </Row>  
       </Container>
     </div>
