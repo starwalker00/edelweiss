376c573149ee909c1b5bdc13e2148b70e3e24515
starwalker00 <aaronwalkerup@gmail.com>
2021-11-18 21:31:25 +0100

    app: styling offcanvas

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -19,25 +19,29 @@ function Navigation(props) {
             </Nav>
           </Navbar.Collapse>
           <Navbar.Offcanvas
-            className="bg-dark"
+            className=""
+            // style={{
+            //   background: "rgba(76, 0, 0, 1)"
+            // }}
             id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel"
             placement="end"
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+              <Offcanvas.Title id="offcanvasNavbarLabel">EDELWEISS</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/menu">Menu</Nav.Link>
+                <Nav.Link href="/schedule">Horaires</Nav.Link>
                 <Nav.Link href="/about">About</Nav.Link>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
       </Navbar>
-    </div>
+    </div >
   );
 }
 
