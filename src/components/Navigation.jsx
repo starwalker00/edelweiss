8f66cca6a42881a8cc7db714c50a099db42fa7ae
starwalker00 <aaronwalkerup@gmail.com>
2021-11-18 18:44:29 +0100

    app: change responsive menu to offcanvas

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,32 +1,39 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Offcanvas } from "react-bootstrap";
 
 function Navigation(props) {
-  const { location } = props;
   return (
     <div className="navigation">
-      <Navbar bg="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">EDELWEISS</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" >
-            <span class="navbar-toggler-icon"></span>
-            <div class="close-icon py-1">✖</div>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav activeKey={location.pathname} className="me-auto">
+      <Navbar collapseOnSelect expand="sm" bg="dark">
+        <Container fluid>
+          <Navbar.Brand href="#">EDELWEISS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Collapse>
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/menu">Menu</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Offcanvas
+            className="bg-dark"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/menu">Menu</Nav.Link>
                 <Nav.Link href="/about">About</Nav.Link>
-              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown> */}
-            </Nav>
-          </Navbar.Collapse>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
         </Container>
       </Navbar>
     </div>
