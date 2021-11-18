0a7a6b44bd845db2a9ef9ff69f4ba0a34314ea3b
starwalker00 <aaronwalkerup@gmail.com>
2021-11-18 19:47:54 +0100

    app: styling horaires

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -4,39 +4,39 @@ import { Container, Row, Col } from "react-bootstrap";
 function Schedule() {
   return (
     <div className="schedule">
-      <Container className="text-center">
+      <Container className="text-center shadow-lg">
         <Row>
-          <Col>Horaires d'ouverture</Col>
+          <Col className="display-6 mt-2">Horaires d'ouverture</Col>
         </Row>
-        <hr/>
-        <Row>
+        <hr />
+        <Row className="py-3">
           <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>lundi</Col>
           <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
         </Row>
-        <Row>
+        <Row className="py-3">
           <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>mardi</Col>
           <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
         </Row>
-        <Row>
+        <Row className="py-3">
           <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>mercredi</Col>
           <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
         </Row>
-        <Row>
+        <Row className="py-3">
           <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>jeudi</Col>
           <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
         </Row>
-        <Row>
+        <Row className="py-3">
           <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>vendredi</Col>
           <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h30</Col>
         </Row>
-        <Row>
+        <Row className="py-3">
           <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>samedi</Col>
           <Col xs={8} md={6} lg={4}>19h30-22h30</Col>
         </Row>
-        <Row>
+        <Row className="py-3">
           <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>dimanche</Col>
           <Col xs={8} md={6} lg={4}>fermé</Col>
-        </Row>        
+        </Row>
       </Container>
     </div>
   );
