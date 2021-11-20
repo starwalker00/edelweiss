ca8ddf9c351a9cd95a50cf9ea4a32121ab14e65c
starwalker00 <aaronwalkerup@gmail.com>
2021-11-20 16:54:19 +0100

    app: fix footer too large

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
       <footer className="pt-4">
         <Container fluid className="text-center text-md-left">
           <Row xs={1} md={2}>
-            <Col className="">
+            <Col>
               <h5 className="text-uppercase">Restaurant l'Edelweiss</h5>
               <p>34 Rue de Saint-Malo, 35000 Rennes
                 <br />
@@ -17,8 +17,7 @@ function Footer() {
                 <br />
               </p>
             </Col>
-
-            <Col className="">
+            <Col>
               <h5 className="text-uppercase">Liens</h5>
               <ul className="list-unstyled">
                 <li><a className="link-secondary" href="/menu"> Menu </a></li>
@@ -27,14 +26,12 @@ function Footer() {
               </ul>
             </Col>
           </Row>
+          <Row>
+            <Col className="footer-copyright text-center py-3">© 2021 Copyright:
+              <a className="link-secondary mx-2" target="_blank" href="https://blank.page/" rel="noreferrer">https://blank.page/</a>
+            </Col>
+          </Row>
         </Container>
-        <Row>
-          <Col className="footer-copyright text-center py-3">© 2021 Copyright:
-            <a className="link-secondary mx-2" target="_blank" href="https://blank.page/" rel="noreferrer">https://blank.page/</a>
-          </Col>
-        </Row>
-
-
       </footer>
     </div>
   );
