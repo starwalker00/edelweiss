432333bd0fd199589d73ba94f5755bf664f80ec8
starwalker00 <aaronwalkerup@gmail.com>
2021-11-20 15:48:00 +0100

    app: footer - filling and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,40 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
-    <div className="footer">
-      <footer className="py-5 bg-dark">
-        <div className="container">
-          <p className="m-0 text-center text-white">
-            Copyright &copy; Your Website 2020
-          </p>
-        </div>
+    <div className="footer bg-dark text-black">
+      <footer className="pt-4">
+        <Container fluid className="text-center text-md-left">
+          <Row xs={1} md={2}>
+            <Col className="">
+              <h5 className="text-uppercase">Restaurant l'Edelweiss</h5>
+              <p>34 Rue de Saint-Malo, 35000 Rennes
+                <br />
+                Réservations par téléphone
+                <br />
+                02 22 22 22 22
+                <br />
+              </p>
+            </Col>
+
+            <Col className="">
+              <h5 className="text-uppercase">Liens</h5>
+              <ul className="list-unstyled">
+                <li><a className="link-secondary" href="/menu"> Menu </a></li>
+                <li><a className="link-secondary" href="/schedule"> Horaires </a></li>
+                <li><a className="link-secondary" href="/contact"> Contact </a></li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+        <Row>
+          <Col className="footer-copyright text-center py-3">© 2021 Copyright:
+            <a className="link-secondary mx-2" target="_blank" href="https://blank.page/" rel="noreferrer">https://blank.page/</a>
+          </Col>
+        </Row>
+
+
       </footer>
     </div>
   );
