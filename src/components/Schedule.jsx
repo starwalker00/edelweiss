e264ed58c6ccfbb7129c1f6db26ab32f14581936
starwalker00 <aaronwalkerup@gmail.com>
2021-11-23 19:44:47 +0100

    restyle horaires

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -4,39 +4,41 @@ import { Container, Row, Col } from "react-bootstrap";
 function Schedule() {
   return (
     <div className="schedule">
-      <Container className="text-center shadow-lg">
+      <Container className="text-center shadow">
         <Row>
           <Col className="display-6 mt-3">Horaires d'ouverture</Col>
         </Row>
         <hr className="mt-2" />
-        <Row className="py-3">
-          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>lundi</Col>
-          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
-        </Row>
-        <Row className="py-3">
-          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>mardi</Col>
-          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
-        </Row>
-        <Row className="py-3">
-          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>mercredi</Col>
-          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
-        </Row>
-        <Row className="py-3">
-          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>jeudi</Col>
-          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
-        </Row>
-        <Row className="py-3">
-          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>vendredi</Col>
-          <Col xs={8} md={6} lg={4}>12h-13h30 | 19h30-22h30</Col>
-        </Row>
-        <Row className="py-3">
-          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>samedi</Col>
-          <Col xs={8} md={6} lg={4}>19h30-22h30</Col>
-        </Row>
-        <Row className="py-3">
-          <Col xs={{ span: 4, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>dimanche</Col>
-          <Col xs={8} md={6} lg={4}>fermé</Col>
-        </Row>
+        <div className="text-capitalize">
+          <Row className="py-3 fs-5">
+            <Col xs={{ span: 3, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>lundi</Col>
+            <Col xs={9} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
+          </Row>
+          <Row className="py-3 fs-5 bg-secondary bg-opacity-25">
+            <Col xs={{ span: 3, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>mardi</Col>
+            <Col xs={9} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
+          </Row>
+          <Row className="py-3 fs-5">
+            <Col xs={{ span: 3, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>mercredi</Col>
+            <Col xs={9} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
+          </Row>
+          <Row className="py-3 fs-5 bg-secondary bg-opacity-25">
+            <Col xs={{ span: 3, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>jeudi</Col>
+            <Col xs={9} md={6} lg={4}>12h-13h30 | 19h30-22h</Col>
+          </Row>
+          <Row className="py-3 fs-5">
+            <Col xs={{ span: 3, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>vendredi</Col>
+            <Col xs={9} md={6} lg={4}>12h-13h30 | 19h30-22h30</Col>
+          </Row>
+          <Row className="py-3 fs-5 bg-secondary bg-opacity-25">
+            <Col xs={{ span: 3, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>samedi</Col>
+            <Col xs={9} md={6} lg={4}>19h30-22h30</Col>
+          </Row>
+          <Row className="py-3 fs-5">
+            <Col xs={{ span: 3, offset: 0 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 3 }}>dimanche</Col>
+            <Col xs={9} md={6} lg={4}>fermé</Col>
+          </Row>
+        </div>
       </Container>
     </div>
   );
